c2b54ae87379c9c654ba5af8d11a0b1396714bd7
Xavier Hanin <xavier.hanin@4sh.fr>
2019-09-10 13:09:06 +0200

    upgrade kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@
 
 plugins {
     // Apply the Kotlin JVM plugin to add support for Kotlin.
-    id("org.jetbrains.kotlin.jvm") version "1.3.41"
+    id("org.jetbrains.kotlin.jvm") version "1.3.50"
 
     // Apply the application plugin to add support for building a CLI application.
     application
