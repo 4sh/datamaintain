214f57276099f5b5e5012d2434b5fca75561f9f8
Xavier Hanin <xavier.hanin@4sh.fr>
2019-09-10 17:54:10 +0200

    add task to check native image

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.io.*
+
 object Versions {
     val junit = "5.5.2"
     val strikt = "0.21.1"
@@ -32,4 +34,24 @@ application {
 graal {
     mainClass("datamaintain.AppKt")
     outputName("datamaintain")
+}
+
+tasks.register<Exec>("checkNative") {
+    dependsOn("nativeImage")
+
+    val expected = "Hello world.\n"
+
+    commandLine = listOf("./build/graal/datamaintain")
+    standardOutput = ByteArrayOutputStream()
+
+    doLast {
+        val out = (standardOutput as ByteArrayOutputStream).toString("UTF-8")
+        if (out != expected) {
+            println("Native image check: NOK")
+            println("output:\n`$out`")
+            throw RuntimeException("Execution of native image failed. Output was:\n$out")
+        } else {
+            println("Native image check: OK")
+        }
+    }
 }
