2a713f1e1b3c6e11f5b1fe9aae4e68c84fb892bb
Xavier Hanin <xavier.hanin@4sh.fr>
2019-09-10 13:23:52 +0200

    use junit and strikt for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+object Versions {
+    val junit = "5.5.2"
+    val strikt = "0.21.1"
+}
+
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.50"
     application
@@ -11,8 +16,11 @@ dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${Versions.junit}")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:${Versions.junit}")
+    testImplementation("io.strikt:strikt-core:${Versions.strikt}")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${Versions.junit}")
 }
 
 application {
