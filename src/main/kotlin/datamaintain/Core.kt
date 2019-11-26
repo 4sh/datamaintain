c74fa1c028bceb399e06f30c6c4cff7ffa2a1aa6
driccio <damien.riccio@4sh.fr>
2019-11-26 14:22:06 +0100

    fix: DTMT-23 Change default sorter

--- src/main/kotlin/datamaintain/Core.kt
+++ src/main/kotlin/datamaintain/Core.kt
@@ -1,14 +1,14 @@
 package datamaintain
 
 import datamaintain.report.ExecutionReport
-import datamaintain.sort.CaseInsensitiveAlphabeticalSorter
+import datamaintain.sort.ByLengthAndCaseInsensitiveAlphabeticalSorter
 
 class Core {
 
     fun run(config: Config): ExecutionReport =
             Scanner(config).scan()
                     .let { scripts -> Filter(config).filter(scripts) }
-                    .let { scripts -> CaseInsensitiveAlphabeticalSorter(config).sort(scripts, Script::identifier) }
+                    .let { scripts -> ByLengthAndCaseInsensitiveAlphabeticalSorter(config).sort(scripts, Script::identifier) }
                     .let { scripts -> Pruner(config).prune(scripts) }
                     .let { scripts -> Executor(config).execute(scripts) }
 
