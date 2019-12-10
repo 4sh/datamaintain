d5c2f48dd8163c5e7453d189e1b072b5c614e81b
nroulon <nicolas.roulon@4sh.fr>
2019-12-10 13:42:02 +0100

    DTMT-18 : KMongo dep removed, replace with Mongo driver

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@ object Versions {
     const val junit = "5.5.2"
     const val strikt = "0.21.1"
     const val mockk = "1.9.3"
-    const val kmongo = "3.11.1"
     const val mongo = "4.0.13"
+    const val mongoDriver = "3.11.2"
     const val clikt = "2.3.0"
 }
