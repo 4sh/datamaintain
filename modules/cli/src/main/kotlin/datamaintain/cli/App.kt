6e0a841fe00d6f5764a5f966b7d297f1a825ad0c
Morgan Renou <mrenou@gmail.com>
2019-12-11 09:40:33 +0100

    DTMT-27 - cli load args as string, config have responsibility to convert/validate values

--- modules/cli/src/main/kotlin/datamaintain/cli/App.kt
+++ modules/cli/src/main/kotlin/datamaintain/cli/App.kt
@@ -5,17 +5,14 @@ import com.github.ajalt.clikt.parameters.options.convert
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.validate
+import datamaintain.core.config.CoreConfigKey
 import datamaintain.core.config.DatamaintainConfig
 import datamaintain.core.runDatamaintain
-import datamaintain.core.script.Tag
+import datamaintain.db.driver.mongo.MongoConfigKey
 import datamaintain.db.driver.mongo.MongoDriverConfig
 import java.io.File
-import java.nio.file.Path
-import java.nio.file.Paths
 import java.util.*
 import kotlin.system.exitProcess
-
-val dbValues = listOf("mongo")
 
 class App : CliktCommand() {
 
@@ -23,27 +20,21 @@ class App : CliktCommand() {
             .convert { File(it) }
             .validate { it.exists() }
 
-    private val path: Path? by option(help = "path to directory containing scripts")
-            .convert { Paths.get(it) }
-            .default(Paths.get("./scripts"))
-            .validate { it.toFile().exists() }
-
-    private val dbType: String by option(help = "db type : ${dbValues.joinToString(",")}")
+    private val dbType: String by option(help = "db type : ${DbType.values().joinToString(",") { v -> v.value }}")
             .default("mongo")
-            .validate { dbValues.contains(it) }
+            .validate { DbType.values().map { v -> v.value }.contains(it) }
 
-    private val identifierRegex: Regex? by option(help = "regex to extract identifier part from scripts")
-            .convert { Regex(it) }
+    private val path: String? by option(help = "path to directory containing scripts")
 
-    private val blacklistedTags: Set<Tag>? by option(help = "tags to blacklist")
-            .convert { it.split(",").map { tag -> Tag(tag) }.toSet() }
+    private val identifierRegex: String? by option(help = "regex to extract identifier part from scripts")
+
+    private val blacklistedTags: String? by option(help = "tags to blacklist")
 
     private val mongoDbName: String? by option(help = "mongo db name")
 
     private val mongoUri: String? by option(help = "mongo uri")
 
-    private val mongoTmpPath: Path? by option(help = "mongo tmp file path")
-            .convert { Paths.get(it) }
+    private val mongoTmpPath: String? by option(help = "mongo tmp file path")
 
     override fun run() {
         try {
@@ -51,6 +42,8 @@ class App : CliktCommand() {
             configFilePath?.let {
                 props.load(it.inputStream())
             }
+
+            overloadPropsFromArgs(props)
 
             val config = loadConfig(props)
 
@@ -62,32 +55,32 @@ class App : CliktCommand() {
         }
     }
 
+    private fun overloadPropsFromArgs(props: Properties) {
+        path?.let { props.put(CoreConfigKey.SCAN_PATH.key, it) }
+        identifierRegex?.let { props.put(CoreConfigKey.SCAN_IDENTIFIER_REGEX.key, it) }
+        blacklistedTags?.let { props.put(CoreConfigKey.TAGS_BLACKLISTED.key, it) }
+        mongoDbName?.let { props.put(MongoConfigKey.DB_MONGO_DBNAME.key, it) }
+        mongoUri?.let { props.put(MongoConfigKey.DB_MONGO_URI.key, it) }
+        mongoTmpPath?.let { props.put(MongoConfigKey.DB_MONGO_TMP_PATH.key, it) }
+    }
+
     private fun loadDriverConfig(props: Properties): MongoDriverConfig {
         return when (dbType) {
-            "mongo" -> MongoDriverConfig.buildConfig(props)
+            DbType.MONGO.value -> MongoDriverConfig.buildConfig(props)
             else -> {
                 echo("dbType $dbType is unknown")
                 exitProcess(0)
             }
-        }.let {
-            it.copy(
-                    dbName = this.mongoDbName ?: it.dbName,
-                    mongoUri = this.mongoUri ?: it.mongoUri,
-                    tmpFilePath = this.mongoTmpPath ?: it.tmpFilePath
-            )
         }
     }
 
     private fun loadConfig(props: Properties): DatamaintainConfig {
         val driverConfig = loadDriverConfig(props)
         return DatamaintainConfig.buildConfig(props, driverConfig)
-                .let {
-                    it.copy(
-                            path = this.path ?: it.path,
-                            identifierRegex = this.identifierRegex ?: it.identifierRegex,
-                            blacklistedTags = this.blacklistedTags ?: it.blacklistedTags
-                    )
-                }
+    }
+
+    enum class DbType(val value: String) {
+        MONGO("mongo")
     }
 }
 
