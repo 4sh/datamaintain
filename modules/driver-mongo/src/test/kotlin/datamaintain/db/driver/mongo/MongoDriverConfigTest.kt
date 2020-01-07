cd8bfc7de8eccec45b5db7391f51917fe91f6a6c
nroulon <nicolas.roulon@4sh.fr>
2020-01-07 11:27:21 +0100

    fixup

--- modules/driver-mongo/src/test/kotlin/datamaintain/db/driver/mongo/MongoDriverConfigTest.kt
+++ modules/driver-mongo/src/test/kotlin/datamaintain/db/driver/mongo/MongoDriverConfigTest.kt
@@ -29,7 +29,7 @@ internal class MongoDriverConfigTest {
 
         expectThat(MongoDriverConfig.buildConfig(props)).and {
             get { dbName }.isEqualTo("newName")
-            get { mongoUri }.isEqualTo("mongo://localhost:27017")
+            get { mongoUri }.isEqualTo("mongodb://localhost:27017")
             get { tmpFilePath }.isEqualTo(Paths.get("/tmp/test"))
         }
     }
