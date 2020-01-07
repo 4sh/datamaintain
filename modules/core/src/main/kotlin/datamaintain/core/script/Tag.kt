2c6c2b4fd0dba54d660062edf399cc5d08dc0f51
Lysoun <30841868+Lysoun@users.noreply.github.com>
2020-01-07 12:23:34 +0100

    feat: DTMT-14 Add pathMatchers to tags to give scripts tags depending on path during scan

--- modules/core/src/main/kotlin/datamaintain/core/script/Tag.kt
+++ modules/core/src/main/kotlin/datamaintain/core/script/Tag.kt
@@ -1,11 +1,21 @@
 package datamaintain.core.script
 
-data class Tag(val name: String) {
+import java.nio.file.Path
+import java.nio.file.PathMatcher
+
+data class Tag(
+        val name: String,
+        val pathMatchers: Set<PathMatcher> = emptySet()
+) {
     infix fun matchedBy(script: ScriptWithContent): Boolean {
         return script.tags.any { it matches this }
     }
 
     infix fun matches(tag: Tag): Boolean {
         return name == tag.name
+    }
+
+    infix fun matchesPath(path: Path): Boolean {
+        return pathMatchers.any { pathMatcher -> pathMatcher.matches(path) }
     }
 }
