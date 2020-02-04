90b807aeda7c0c0bad528fdb579f2e5e95c030a3
Morgan Renou <mrenou@gmail.com>
2020-02-04 12:20:30 +0100

    DTMT-30 - refactor test of correct execution in report

--- modules/core/src/main/kotlin/datamaintain/core/report/ExecutionReport.kt
+++ modules/core/src/main/kotlin/datamaintain/core/report/ExecutionReport.kt
@@ -1,16 +1,12 @@
 package datamaintain.core.report
 
-import datamaintain.core.script.ExecutionStatus
 import java.time.Instant
 
 class ExecutionReport(override val lines: List<ExecutionLineReport>, override val date: Instant) : Report {
     override val status: ReportStatus
-        get() = if (lines.all { executionLineReport ->
-                    executionLineReport.executionStatus == ExecutionStatus.OK ||
-                            executionLineReport.executionStatus == ExecutionStatus.FORCE_MARKED_AS_EXECUTED ||
-                            executionLineReport.executionStatus == ExecutionStatus.SHOULD_BE_EXECUTED
-                })
+        get() = if (lines.all { executionLineReport -> executionLineReport.executionStatus.correctlyExecuted() }) {
             ReportStatus.OK
-        else
+        } else {
             ReportStatus.KO
+        }
 }
