40139c6d84351a07d0c9180898fb17800381ad2b
Élise Rubio <30841868+Lysoun@users.noreply.github.com>
2020-04-28 10:44:31 +0200

    refactor: #54 Extract connection building to class

---
+++ modules/driver-mongo/src/main/kotlin/datamaintain/db/driver/mongo/ConnectionString.kt
@@ -0,0 +1,26 @@
+package datamaintain.db.driver.mongo
+
+class ConnectionString {
+    companion object {
+        private val MONGO_URI_REGEX = Regex("^mongodb(?:\\+srv)?://(?:([-._\\w]+):(.+)@)?([-\\w]+)(?::([0-9]+))?(?:/([\\w-_.]+)?(?:\\?([\\w_.]+=[\\w_]+))?)?")
+
+        @JvmStatic
+        fun buildConnectionString(mongoUri: String): String {
+            val matchResult = MONGO_URI_REGEX.matchEntire(mongoUri)
+                    ?: throw IllegalArgumentException("MongoUri does not contains a database name")
+
+
+            val (_, _, host, port, username, password, database) = matchResult.destructured
+//        val connectionString = ConnectionString(mongoUri)
+//        // mongoUri can come with a database but currently driver's dbName is mandatory
+//        if (connectionString.database == null) {
+//            throw IllegalArgumentException("MongoUri does not contains a database name")
+//        }
+//        // mongoUri can come with a collection. It has no sense in DataMaintain's logic
+//        if (connectionString.collection != null) {
+//            throw IllegalArgumentException("MongoUri contains a collection name, please remove it")
+//        }
+            return mongoUri
+        }
+    }
+}
