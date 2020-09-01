959153699b73244a80e25828b32873f07cac9bb1
driccio <damien.riccio@4sh.fr>
2020-09-01 16:50:37 +0200

    fix: #75 Add some missing test cases and correct algorithm

--- modules/core/src/main/kotlin/datamaintain/core/step/sort/ByCaseInsensitiveSeparatorFreeAlphabeticalSortingStrategy.kt
+++ modules/core/src/main/kotlin/datamaintain/core/step/sort/ByCaseInsensitiveSeparatorFreeAlphabeticalSortingStrategy.kt
@@ -2,7 +2,17 @@ package datamaintain.core.step.sort
 
 import datamaintain.core.script.Script
 
+/**
+ * Allow to sort scripts considering the parts of the value returned by the getter.
+ * The parts list is given by a split on all chars that are not a figure or a letter.
+ * The number parts would be sorted as number, and the others as string (so with alphabetical sort)
+ *
+ * We assume the differents values have an homogeneity. For instance, you will have troubles
+ * to sort ["abc.123", "abc.234", "abc.0001", "abc.def"]
+ */
 class ByCaseInsensitiveSeparatorFreeAlphabeticalSortingStrategy : SortingStrategy<String>() {
+    private val numberRegex = "^\\d*$".toRegex()
+
     override fun <T : Script> sort(scripts: List<T>, getter: (T) -> String): List<T> {
         return scripts.sortedWith(Comparator { script1, script2 ->
             val value1 = split(getter(script1))
@@ -12,7 +22,7 @@ class ByCaseInsensitiveSeparatorFreeAlphabeticalSortingStrategy : SortingStrateg
 
             for ((index, part1) in value1.withIndex()) {
                 if (value2.size <= index) {
-                    // value1 is after value2
+                    // value1 is after value2 because value1 has more parts than value2
                     compareResult = 1;
                     break
                 } else {
@@ -21,13 +31,20 @@ class ByCaseInsensitiveSeparatorFreeAlphabeticalSortingStrategy : SortingStrateg
                     val lengthComparison = part1.length.compareTo(part2.length)
 
                     if (lengthComparison != 0) {
-                        compareResult = lengthComparison
-                        break
+                        // The length are not equals.
+
+                        // When part are number, then we can simply sort them assuming their length
+                        if (numberRegex.matches(part1)) {
+                            // The part having the smallest size is before
+                            compareResult = lengthComparison
+                            break
+                        }
                     }
 
                     val compareTo = part1.compareTo(part2)
 
                     if (compareTo != 0) {
+                        // The 2 part are not equals so we know the order of the scripts
                         compareResult = compareTo;
                         break
                     }
@@ -35,7 +52,8 @@ class ByCaseInsensitiveSeparatorFreeAlphabeticalSortingStrategy : SortingStrateg
             }
 
             if (compareResult == 0 && value1.size != value2.size) {
-                // value2 is after value1
+                // The last case is when value2 is longer than value1. It means value2 has more parts than value1,
+                // so value2 is after value1
                 compareResult = -1;
             }
 
