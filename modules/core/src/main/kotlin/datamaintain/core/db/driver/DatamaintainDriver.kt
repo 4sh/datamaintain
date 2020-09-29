ebed88ed46fe10ce80330214a7aaa3f592a246bf
Élise Rubio <30841868+Lysoun@users.noreply.github.com>
2020-09-29 18:26:13 +0200

    docs: Add comments in driver interfaces

--- modules/core/src/main/kotlin/datamaintain/core/db/driver/DatamaintainDriver.kt
+++ modules/core/src/main/kotlin/datamaintain/core/db/driver/DatamaintainDriver.kt
@@ -6,9 +6,18 @@ import datamaintain.core.step.executor.Execution
 
 interface DatamaintainDriver {
 
+    /**
+     * Reads the executed scripts from the database and returns them
+     */
     fun listExecutedScripts(): Sequence<ExecutedScript>
 
+    /**
+     * Executes the given script and inserts its execution in the database
+     */
     fun executeScript(script: ScriptWithContent): Execution
 
+    /**
+     * Does not execute the given script, only inserts its execution in the database
+     */
     fun markAsExecuted(executedScript: ExecutedScript): ExecutedScript
 }
--- modules/core/src/main/kotlin/datamaintain/core/db/driver/DatamaintainDriverConfig.kt
+++ modules/core/src/main/kotlin/datamaintain/core/db/driver/DatamaintainDriverConfig.kt
@@ -2,7 +2,13 @@ package datamaintain.core.db.driver
 
 interface DatamaintainDriverConfig {
 
+    /**
+     * Builds a driver based on the configuration
+     */
     fun toDriver(): DatamaintainDriver
 
+    /**
+     * Logs with an info level the driver configuration
+     */
     fun log()
 }
