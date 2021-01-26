0ddeec35c1fb151170efe02c5f0b1db30320523f
Élise Rubio <30841868+Lysoun@users.noreply.github.com>
2021-01-26 13:54:15 +0100

    :white_check_mark: #114 Fix test name

--- modules/cli/src/test/kotlin/datamaintain/cli/AppTest.kt
+++ modules/cli/src/test/kotlin/datamaintain/cli/AppTest.kt
@@ -244,7 +244,7 @@ internal class AppTest {
                 @Nested
                 inner class MongoSaveOutput {
                     @Test
-                    fun `should build config with create tags from folder set to true`() {
+                    fun `should build config with mongo save output set to true`() {
                         // Given
                         val argv = updateMongoDbMinimumArguments().plus("--mongo-save-output")
 
@@ -258,7 +258,7 @@ internal class AppTest {
                     }
 
                     @Test
-                    fun `should build config with create tags from folder set to false`() {
+                    fun `should build config with mongo save output set to false`() {
                         // Given
                         val argv = updateMongoDbMinimumArguments()
 
