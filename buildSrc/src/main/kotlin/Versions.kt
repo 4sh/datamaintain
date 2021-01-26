f36ba5793ecf620c1b0701ae4deb171a63e5c587
Élise Rubio <30841868+Lysoun@users.noreply.github.com>
2021-01-26 11:30:25 +0100

    :white_check_mark: #114 Test config build with execution mode

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,7 +2,7 @@ object Versions {
     const val kotlin = "1.3.70"
     const val kotlinLogging = "1.7.7"
     const val logbackClassic = "1.2.3"
-    const val junit = "5.5.2"
+    const val junit = "5.7.0"
     const val strikt = "0.21.1"
     const val mockk = "1.9.3"
     const val mongo = "4.0.13"
