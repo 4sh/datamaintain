b54f093564fe591d355ddfc1b5b0a4840279719e
Élise Rubio <30841868+Lysoun@users.noreply.github.com>
2021-04-06 10:22:13 +0200

    :bug: #117 No longer throw exception when mongo URI contains multiple options

--- modules/driver-mongo/src/main/kotlin/datamaintain/db/driver/mongo/MongoConnectionStringBuilder.kt
+++ modules/driver-mongo/src/main/kotlin/datamaintain/db/driver/mongo/MongoConnectionStringBuilder.kt
@@ -4,5 +4,5 @@ import datamaintain.core.db.driver.ConnectionStringBuilder
 import datamaintain.db.driver.mongo.exception.DatamaintainMongoUriException
 
 class MongoConnectionStringBuilder: ConnectionStringBuilder(
-        "^mongodb(?:\\+srv)?://(?:([-._\\w]+):(.+)@)?([-.\\w]+)(?::([0-9]+))?(?:/([\\w-_]+)(?:\\?([\\w_.]+=[\\w_]+))?)",
+        "^mongodb(?:\\+srv)?://(?:([-._\\w]+):(?:.+)@)?(?:[-.\\w]+)(?::(?:[0-9]+))?(?:/([\\w-_]+)(?:\\?(?:[\\w_.]+=[\\w_]+)(?:&[\\w]+=[\\w-\\d,:]+)*)?)",
         {uri -> DatamaintainMongoUriException(uri) })
