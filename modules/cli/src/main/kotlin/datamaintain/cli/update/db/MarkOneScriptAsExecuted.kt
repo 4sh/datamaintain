b17ba2c19f66b10bcd2e42280b62c6ae5a1cabf6
Élise Rubio <30841868+Lysoun@users.noreply.github.com>
2021-05-04 10:45:04 +0200

    :sparkles: #38 Mark one script as executed

---
+++ modules/cli/src/main/kotlin/datamaintain/cli/update/db/MarkOneScriptAsExecuted.kt
@@ -0,0 +1,26 @@
+package datamaintain.cli.update.db
+
+import com.github.ajalt.clikt.parameters.options.flag
+import com.github.ajalt.clikt.parameters.options.option
+import datamaintain.core.config.CoreConfigKey
+import datamaintain.core.config.DatamaintainConfig
+import datamaintain.core.script.ScriptAction
+import java.util.*
+
+class MarkOneScriptAsExecuted(runner: (DatamaintainConfig) -> Unit = ::defaultUpdateDbRunner) :
+    DatamaintainCliUpdateDbCommand(
+        name = "mark-script-as-executed",
+        runner = runner
+    ) {
+    private val path: String? by option(help = "path to the script you want to mark as executed")
+
+    private val verbose: Boolean? by option(help = "verbose").flag()
+
+    override fun overloadProps(props: Properties) {
+        props[CoreConfigKey.DEFAULT_SCRIPT_ACTION.key] = ScriptAction.MARK_AS_EXECUTED.name
+
+        // Overload from arguments
+        path?.let { props.put(CoreConfigKey.SCAN_PATH.key, it) }
+        verbose?.let { props.put(CoreConfigKey.VERBOSE.key, it.toString()) }
+    }
+}
