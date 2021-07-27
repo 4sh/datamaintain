bc6a908df8b4e7a02380c215675bbd3222222b32
asolovieff <alexandre.solovieff@4sh.fr>
2021-07-27 12:14:01 +0200

    feat #147 ReportExecutedScript
    ExecutedScript is no longer a data class so that the new class ReportExecutedScript can inherit from it. Methods equals(), hashCode() et toString() ajoutées après le retrait du mot clef data.

--- modules/core/src/main/kotlin/datamaintain/core/script/Script.kt
+++ modules/core/src/main/kotlin/datamaintain/core/script/Script.kt
@@ -12,14 +12,14 @@ interface Script {
     }
 }
 
-data class ExecutedScript @JvmOverloads constructor(
-        override val name: String,
-        override val checksum: String,
-        override val identifier: String,
-        val executionStatus: ExecutionStatus,
-        var action: ScriptAction? = null,
-        val executionDurationInMillis: Long? = null,
-        val executionOutput: String? = null
+open class ExecutedScript @JvmOverloads constructor(
+    override val name: String,
+    override val checksum: String,
+    override val identifier: String,
+    open val executionStatus: ExecutionStatus,
+    open var action: ScriptAction? = null,
+    open val executionDurationInMillis: Long? = null,
+    open val executionOutput: String? = null
 ) : Script {
     companion object {
         fun simulateExecuted(script: ScriptWithContent, executionStatus: ExecutionStatus) =
@@ -44,7 +44,58 @@ data class ExecutedScript @JvmOverloads constructor(
                         execution.executionOutput
                 )
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ExecutedScript
+
+        if (name != other.name) return false
+        if (checksum != other.checksum) return false
+        if (identifier != other.identifier) return false
+        if (executionStatus != other.executionStatus) return false
+        if (action != other.action) return false
+        if (executionDurationInMillis != other.executionDurationInMillis) return false
+        if (executionOutput != other.executionOutput) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + checksum.hashCode()
+        result = 31 * result + identifier.hashCode()
+        result = 31 * result + executionStatus.hashCode()
+        result = 31 * result + (action?.hashCode() ?: 0)
+        result = 31 * result + (executionDurationInMillis?.hashCode() ?: 0)
+        result = 31 * result + (executionOutput?.hashCode() ?: 0)
+        return result
+    }
+
+    override fun toString(): String {
+        return "ExecutedScript(name='$name', checksum='$checksum', identifier='$identifier', executionStatus=$executionStatus, action=$action, executionDurationInMillis=$executionDurationInMillis, executionOutput=$executionOutput)"
+    }
 }
+
+data class ReportExecutedScript(
+    override val name: String,
+    override val checksum: String,
+    override val identifier: String,
+    override val executionStatus: ExecutionStatus,
+    override var action: ScriptAction? = null,
+    override val executionDurationInMillis: Long? = null,
+    override val executionOutput: String? = null,
+    val porcelainName: String
+) : ExecutedScript(
+        name,
+        checksum,
+        identifier,
+        executionStatus,
+        action,
+        executionDurationInMillis,
+        executionOutput
+)
 
 interface ScriptWithContent : Script {
     val content: String
