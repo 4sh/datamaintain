a875938e5af3370117fd62de1f64e010a4f1d588
Élise Rubio <30841868+Lysoun@users.noreply.github.com>
2021-08-10 14:08:04 +0200

    :white_check_mark: #147 Test porcelain name logging

---
+++ modules/core/src/test/kotlin/datamaintain/core/report/ReportTest.kt
@@ -0,0 +1,91 @@
+package datamaintain.core.report
+
+import ch.qos.logback.classic.Logger
+import datamaintain.test.TestAppender
+import datamaintain.test.buildReportExecutedScript
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+
+
+internal class ReportTest {
+    private val logger = LoggerFactory.getLogger("datamaintain.core.report.Report") as Logger
+    private val testAppender = TestAppender()
+
+    @BeforeEach
+    fun setupLogger() {
+        logger.addAppender(testAppender)
+        testAppender.start()
+    }
+
+    @Nested
+    inner class ExecuteLogs {
+        @Test
+        fun `should display relative paths when porcelain is true`() {
+            // Given
+            val report = Report(
+                executedScripts = listOf(
+                    buildReportExecutedScript(
+                        "script1",
+                        "porcelainName1"
+                    ), buildReportExecutedScript(
+                        "script2",
+                        "porcelainName2"
+                    )
+                )
+            )
+
+            // When
+            report.print(verbose = true, porcelain = true)
+
+            // Then
+            expectThat(testAppender.events) {
+                get { get(6).message }.isEqualTo(
+                    " -> script1 - porcelainName1"
+                )
+                get { get(7).message }.isEqualTo(
+                    " -> script2 - porcelainName2"
+                )
+            }
+        }
+
+        @Test
+        fun `should not display relative paths when porcelain is false`() {
+            // Given
+            val report = Report(
+                executedScripts = listOf(
+                    buildReportExecutedScript(
+                        "script1",
+                        "porcelainName1"
+                    ), buildReportExecutedScript(
+                        "script2",
+                        "porcelainName2"
+                    )
+                )
+            )
+
+            // When
+            report.print(verbose = true, porcelain = false)
+
+            // Then
+            expectThat(testAppender.events) {
+                get { get(6).message }.isEqualTo(
+                    " -> script1"
+                )
+                get { get(7).message }.isEqualTo(
+                    " -> script2"
+                )
+            }
+        }
+    }
+
+    @AfterEach
+    fun stopTestAppender() {
+        testAppender.stop()
+        testAppender.clearEvents()
+    }
+}
---
+++ modules/core/src/test/kotlin/datamaintain/test/ScriptBuildingUtils.kt
@@ -0,0 +1,13 @@
+package datamaintain.test
+
+import datamaintain.core.script.ExecutionStatus
+import datamaintain.core.script.ReportExecutedScript
+
+fun buildReportExecutedScript(scriptName: String, porcelainName: String) =
+    ReportExecutedScript(
+        name = scriptName,
+        porcelainName = porcelainName,
+        checksum = "",
+        identifier = "",
+        executionStatus = ExecutionStatus.OK
+    )
