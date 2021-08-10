286dddfdbb1aa0438b91921730c0141079615dbb
Élise Rubio <30841868+Lysoun@users.noreply.github.com>
2021-08-10 10:10:58 +0200

    :white_check_mark: #147 Test relative path extraction

---
+++ modules/core/src/test/kotlin/datamaintain/core/util/PorcelainUtilTest.kt
@@ -0,0 +1,34 @@
+package datamaintain.core.util
+
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+import java.nio.file.Paths
+
+internal class PorcelainUtilTest {
+    @Test
+    fun should_extract_relative_path_without_start_slash() {
+        // Given
+        val scanPath = Paths.get("/scan/path")
+        val filePath = Paths.get("/scan/path/files/my_file")
+
+        // When
+        val actualRelativePath = extractRelativePath(scanPath = scanPath, filePath = filePath)
+
+        // Then
+        expectThat(actualRelativePath).isEqualTo("files/my_file")
+    }
+
+    @Test
+    fun should_extract_relative_path() {
+        // Given
+        val scanPath = Paths.get("/scan/path/")
+        val filePath = Paths.get("/scan/path/files/my_file")
+
+        // When
+        val actualRelativePath = extractRelativePath(scanPath = scanPath, filePath = filePath)
+
+        // Then
+        expectThat(actualRelativePath).isEqualTo("files/my_file")
+    }
+}
