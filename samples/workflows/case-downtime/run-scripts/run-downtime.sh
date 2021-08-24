4866717c9ae31393f62c1ab216adab47d36781dc
driccio <damien.riccio@4sh.fr>
2021-08-24 09:15:16 +0200

    :memo: #99 Rename DURING into DOWNTIME

--- samples/workflows/case-downtime/run-scripts/run-downtime.sh
+++ samples/workflows/case-downtime/run-scripts/run-downtime.sh
@@ -1,5 +1,5 @@
 docker run --rm --volume $PATH_TO_DATAMAINTAIN_PROJECT/samples/workflows/case-downtime:/case-downtime --network=host docker.pkg.github.com/4sh/datamaintain/datamaintain:2.0-mongo-4.4 \
 --config-file-path /case-downtime/datamaintain.properties \
 update-db \
---tag DURING=/case-downtime/scripts/*_DURING.js \
---whitelisted-tags DURING \
+--tag DOWNTIME=/case-downtime/scripts/*_DOWNTIME.js \
+--whitelisted-tags DOWNTIME \
