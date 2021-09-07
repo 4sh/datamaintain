63db27b471b7a3b3051805c5b88f83207d419ed8
nroulon <nicolas.roulon@4sh.fr>
2021-09-07 10:57:13 +0200

    feat: add test for TagMatcher matching

--- modules/core/src/test/kotlin/datamaintain/core/script/TagMatcherTest.kt
+++ modules/core/src/test/kotlin/datamaintain/core/script/TagMatcherTest.kt
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test
 import strikt.api.expectCatching
 import strikt.api.expectThat
 import strikt.assertions.*
+import java.nio.file.Paths
 
 internal class TagMatcherTest {
     @Nested
@@ -112,6 +113,88 @@ internal class TagMatcherTest {
                 // When
                 expectCatching { TagMatcher.parse(tagName, "[$pathMatcher]") }
                         .succeeded()
+            }
+        }
+
+        @Nested
+        inner class Match {
+            @Test
+            fun `should match`() {
+                // Given
+                var scanPathString = "src/test/resources/scanner_test_files"
+                val scanPath = Paths.get(scanPathString).toAbsolutePath()
+                scanPathString = scanPath.toString()
+
+                val pathToFile = "$scanPathString/01_file1"
+                val pathMatchersString = "[$pathToFile]"
+
+                val tagName = "TOTO"
+                val tagMatcher = TagMatcher.parse(tagName, pathMatchersString, scanPath)
+
+                // When
+                val tagMatch = tagMatcher.matches(Paths.get(pathToFile))
+
+                // Then
+                expectThat(tagMatch).isTrue()
+            }
+
+            @Test
+            fun `should match with glob`() {
+                // Given
+                var scanPathString = "src/test/resources/scanner_test_files"
+                val scanPath = Paths.get(scanPathString).toAbsolutePath()
+                scanPathString = scanPath.toString()
+
+                val pathToFile = "$scanPathString/01_file1"
+                val pathMatchersString = "[$scanPathString/*file1]"
+
+                val tagName = "TOTO"
+                val tagMatcher = TagMatcher.parse(tagName, pathMatchersString, scanPath)
+
+                // When
+                val tagMatch = tagMatcher.matches(Paths.get(pathToFile))
+
+                // Then
+                expectThat(tagMatch).isTrue()
+            }
+
+            @Test
+            fun `should not match`() {
+                // Given
+                var scanPathString = "src/test/resources/scanner_test_files"
+                val scanPath = Paths.get(scanPathString).toAbsolutePath()
+                scanPathString = scanPath.toString()
+
+                val pathMatcher = "$scanPathString/01_file1"
+                val pathToFile = "$scanPathString/01_file2"
+                val pathMatchersString = "[$pathMatcher]"
+                val tagName = "TOTO"
+                val tagMatcher = TagMatcher.parse(tagName, pathMatchersString, scanPath)
+
+                // When
+                val tagMatch = tagMatcher.matches(Paths.get(pathToFile))
+
+                // Then
+                expectThat(tagMatch).isFalse()
+            }
+
+            @Test
+            fun `should not match with glob`() {
+                // Given
+                var scanPathString = "src/test/resources/scanner_test_files"
+                val scanPath = Paths.get(scanPathString).toAbsolutePath()
+                scanPathString = scanPath.toString()
+
+                val pathMatcher = "$scanPathString/01_file1"
+                val pathMatchersString = "[$scanPathString/*file2]"
+                val tagName = "TOTO"
+                val tagMatcher = TagMatcher.parse(tagName, pathMatchersString, scanPath)
+
+                // When
+                val tagMatch = tagMatcher.matches(Paths.get(pathMatcher))
+
+                // Then
+                expectThat(tagMatch).isFalse()
             }
         }
     }
