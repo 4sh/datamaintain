5c0e65dbabe63606f8cc5328e90413bfa270b1b6
asolovieff <alexandre.solovieff@4sh.fr>
2021-09-07 10:22:25 +0200

    feat #147 Add a builder to create a DatamaintainConfig for the tests

---
+++ modules/core/src/test/kotlin/datamaintain/test/DatamaintainConfigBuildingUtils.kt
@@ -0,0 +1,44 @@
+package datamaintain.test
+
+import datamaintain.core.config.CoreConfigKey
+import datamaintain.core.config.DatamaintainConfig
+import datamaintain.core.db.driver.DatamaintainDriverConfig
+import datamaintain.core.db.driver.FakeDriverConfig
+import datamaintain.core.script.ScriptAction
+import datamaintain.core.script.Tag
+import datamaintain.core.script.TagMatcher
+import datamaintain.core.step.executor.ExecutionMode
+import java.nio.file.Path
+import java.nio.file.Paths
+
+fun buildDatamaintainConfig(
+    path: Path = Paths.get(CoreConfigKey.SCAN_PATH.default!!),
+    identifierRegex: Regex = Regex(CoreConfigKey.SCAN_IDENTIFIER_REGEX.default!!),
+    doesCreateTagsFromFolder: Boolean = CoreConfigKey.CREATE_TAGS_FROM_FOLDER.default!!.toBoolean(),
+    whitelistedTags: Set<Tag> = setOf(),
+    blacklistedTags: Set<Tag> = setOf(),
+    tagsToPlayAgain: Set<Tag> = setOf(),
+    overrideExecutedScripts: Boolean = CoreConfigKey.PRUNE_OVERRIDE_UPDATED_SCRIPTS.default!!.toBoolean(),
+    tagsMatchers: Set<TagMatcher> = setOf(),
+    checkRules: Sequence<String> = emptySequence(),
+    executionMode: ExecutionMode = ExecutionMode.NORMAL,
+    defaultScriptAction: ScriptAction = DatamaintainConfig.defaultAction,
+    driverConfig: DatamaintainDriverConfig = FakeDriverConfig(),
+    verbose: Boolean = CoreConfigKey.VERBOSE.default!!.toBoolean(),
+    porcelain: Boolean = CoreConfigKey.PRINT_RELATIVE_PATH_OF_SCRIPT.default!!.toBoolean()
+) = DatamaintainConfig(
+    path,
+    identifierRegex,
+    doesCreateTagsFromFolder,
+    whitelistedTags,
+    blacklistedTags,
+    tagsToPlayAgain,
+    overrideExecutedScripts,
+    tagsMatchers,
+    checkRules,
+    executionMode,
+    defaultScriptAction,
+    driverConfig,
+    verbose,
+    porcelain
+)
