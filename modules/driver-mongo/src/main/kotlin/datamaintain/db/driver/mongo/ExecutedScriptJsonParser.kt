bbd4ec289f5d40adfafe579104b60d3b2b97aa96
asolovieff <alexandre.solovieff@4sh.fr>
2021-10-05 11:50:09 +0200

    refacto #143 Removed useless methods in driver-mongo
    ExecutedScript are only serialized and LightExecutedScript are only deserialized.

--- modules/driver-mongo/src/main/kotlin/datamaintain/db/driver/mongo/ExecutedScriptJsonParser.kt
+++ modules/driver-mongo/src/main/kotlin/datamaintain/db/driver/mongo/ExecutedScriptJsonParser.kt
@@ -3,9 +3,6 @@ package datamaintain.db.driver.mongo
 import datamaintain.core.script.ExecutedScript
 
 interface ExecutedScriptJsonParser {
-    // parse a stringify json array to an Array of ExecutedScript
-    fun parseArrayOfExecutedScripts(executedScriptJsonArray: String): Sequence<ExecutedScript>
-
     // Serialize an ExecutedScript to a stringify json document
     fun serializeExecutedScript(executedScript:ExecutedScript): String
 }
