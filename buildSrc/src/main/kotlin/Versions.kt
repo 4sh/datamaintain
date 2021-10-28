584598cfad13e7b765d6c46ac4360a63fb951beb
Zubin Paul <zubinpaul@outlook.com>
2021-10-28 16:50:18 +0530

    Grouped Dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,11 +5,13 @@ object Versions {
     const val junit = "5.7.0"
     const val strikt = "0.21.1"
     const val mockk = "1.9.3"
+    const val clikt = "2.3.0"
+
     const val mongo = "4.0.13"
     const val mongoDriver = "3.11.2"
-    const val clikt = "2.3.0"
-    const val h2Database = "1.4.200"
-    const val kotlinxSerializationRuntime = "0.20.0"
     const val jongo = "1.4.1"
+    const val kotlinxSerializationRuntime = "0.20.0"
+
+    const val h2Database = "1.4.200"
     const val postgresql = "42.1.4"
 }
