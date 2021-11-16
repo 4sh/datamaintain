80fb3666cdcc46ed19111b4d26feae9438deee5e
nroulon <nicolas.roulon@4sh.fr>
2021-11-16 11:58:24 +0100

    chore: #172 upgrade gradle

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -4,7 +4,7 @@ import org.gradle.kotlin.dsl.dependencies
 
 fun Project.kotlinProject() {
     dependencies {
-        "implementation"(platform("org.jetbrains.kotlin:kotlin-bom:${Versions.kotlin}@pom"))
+        "api"((platform("org.jetbrains.kotlin:kotlin-bom:${Versions.kotlin}")))
         "implementation"("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     }
 }
