beb2ac6a7db58c777735cb68dbbcf54d0378f43b
nroulon <nicolas.roulon@4sh.fr>
2022-01-25 12:43:49 +0100

    feat: #161 add docker image for mongo 5.0

--- modules/cli/docker-build-push.sh
+++ modules/cli/docker-build-push.sh
@@ -9,6 +9,7 @@ image='docker.pkg.github.com/4sh/datamaintain/datamaintain'
 mongo_3_2_dockerfile=$script_dir/Dockerfile/mongo/3/3.2/Dockerfile  # DockerFile with mongo 3.2 to 3.4
 mongo_3_6_dockerfile=$script_dir/Dockerfile/mongo/3/3.6/Dockerfile  # DockerFile with mongo 3.6
 mongo_4_dockerfile=$script_dir/Dockerfile/mongo/4/Dockerfile  # DockerFile with mongo 4.0 to 4.4
+mongo_5_dockerfile=$script_dir/Dockerfile/mongo/5/Dockerfile  # DockerFile with mongo 5.0
 
 docker_build_datamaintain() {
   image_build="$image:$datamaintain_version-mongo-$2"
@@ -23,3 +24,4 @@ docker_build_datamaintain "$mongo_3_6_dockerfile" 3.6
 docker_build_datamaintain "$mongo_4_dockerfile" 4.0
 docker_build_datamaintain "$mongo_4_dockerfile" 4.2
 docker_build_datamaintain "$mongo_4_dockerfile" 4.4
+docker_build_datamaintain "$mongo_5_dockerfile" 5.0
