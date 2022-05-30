5c45feec65c995ed84b68ca3a2f413d854eb20e1
nroulon <nicolas.roulon@4sh.fr>
2022-05-30 13:54:39 +0200

    feat: #174 add Jackson implementation of JsonMapper

---
+++ modules/driver-mongo-mapping/driver-mongo-mapping-jackson/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    kotlin("plugin.serialization") version Versions.kotlin
+    `maven-publish` // Needed for Jitpack
+    id("com.adarshr.test-logger")
+}
+
+baseProject()
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly(project(":modules:core"))
+    compileOnly(project(":modules:driver-mongo"))
+    compileOnly("com.fasterxml.jackson.core:jackson-databind:${Versions.versionLatest}")
+
+    testImplementation(project(":modules:core"))
+    testImplementation(project(":modules:driver-mongo"))
+    testImplementation(project(":modules:driver-mongo-mapping:driver-mongo-mapping-test"))
+    testImplementation("com.fasterxml.jackson.core:jackson-databind:${Versions.versionLatest}")
+}
