a108fc2b1de9af87a747eb2029ac06f9eb7df70a
Élise Rubio <30841868+Lysoun@users.noreply.github.com>
2022-07-26 15:23:21 +0200

    :alembic: Send reports using Monitoring API

---
+++ modules/monitoring/src/main/kotlin/datamaintain/monitoring/ReportSender.kt
@@ -0,0 +1,22 @@
+package datamaintain.monitoring
+
+import MonitoringReport
+import com.fasterxml.jackson.databind.ObjectMapper
+import datamaintain.domain.report.IReportSender
+import datamaintain.domain.report.Report
+import org.http4k.client.Java8HttpClient
+import org.http4k.core.Method
+import org.http4k.core.Request
+import org.http4k.format.ConfigurableJackson
+
+val url = "http://localhost:8080"
+
+class ReportSender : IReportSender {
+    private val httpClient = Java8HttpClient()
+    override fun sendReport(report: Report) {
+        httpClient(Request(Method.POST, url).body(ConfigurableJackson(ObjectMapper()).asFormatString(report.toMonitoringReport())))
+    }
+}
+
+fun Report.toMonitoringReport() =
+    MonitoringReport(this.executedScripts.size)
