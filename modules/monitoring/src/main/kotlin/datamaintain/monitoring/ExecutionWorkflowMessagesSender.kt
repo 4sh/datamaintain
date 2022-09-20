75b6ed4d729d561d41e043ade561a0c7ff166eee
Élise Rubio <30841868+Lysoun@users.noreply.github.com>
2022-09-20 14:52:21 +0200

    :truck: Rename sender

--- modules/monitoring/src/main/kotlin/datamaintain/monitoring/ExecutionWorkflowMessagesSender.kt
+++ modules/monitoring/src/main/kotlin/datamaintain/monitoring/ExecutionWorkflowMessagesSender.kt
@@ -1,15 +1,13 @@
 package datamaintain.monitoring
 
 import MonitoringReport
-import com.fasterxml.jackson.databind.ObjectMapper
-import datamaintain.domain.report.IReportSender
+import datamaintain.domain.report.IExecutionWorkflowMessagesSender
 import datamaintain.domain.report.Report
 import org.http4k.client.Java8HttpClient
 import org.http4k.core.Method
 import org.http4k.core.Request
-import org.http4k.format.ConfigurableJackson
 
-class ReportSender : IReportSender {
+class ExecutionWorkflowMessagesSender : IExecutionWorkflowMessagesSender {
     private val httpClient = Java8HttpClient()
     private val executionApiBaseUrl = "$baseUrl/public/executions"
 
