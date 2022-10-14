f00a2adb556798ced5ac8999c8693eb69ae0421d
Frank Schmitt <frankschmitt@gmx.de>
2022-10-14 23:31:43 +0200

    #70: write meaningful unit tests for auto-completion generation
    We use a custom version of execAppInSubprocess() that directly writes the output to a file. This avoids a messed-up console when generating the scripts.

--- modules/cli/src/main/kotlin/datamaintain/cli/completion/RebuildAutoCompletionScripts.kt
+++ modules/cli/src/main/kotlin/datamaintain/cli/completion/RebuildAutoCompletionScripts.kt
@@ -1,14 +1,15 @@
 package datamaintain.cli.completion
 
-import datamaintain.core.util.execAppInSubprocess
 import java.io.File
+import java.io.IOException
+
+data class ProcessResult(val exitValue: Int, val output: String)
 
 fun generateAutoCompletionScripts(outputDir: String) {
-    System.out.println("Rebuilding auto-completion scripts in " + outputDir)
-
     listOf("bash", "zsh").forEach {
-        val result = execAppInSubprocess(listOf("--generate-completion", it))
-        File(outputDir + "/" + it + "-autocomplete.sh").writeText(result.output)
+        val result = execAppInSubprocess(listOf("--generate-completion", it),
+                                 outputDir + "/" + it + "-autocomplete.sh")
+        //File(outputDir + "/" + it + "-autocomplete.sh").writeText(result.output)
     }
 
 }
@@ -17,3 +18,33 @@ fun main() {
     val outputDir = "../../docs/auto-completion"
     generateAutoCompletionScripts(outputDir)
  }
+
+/**
+ * Execute App.kt class as a subprocess. The class is executed with the "java" command.
+ * This allow the subprocess to call "exit 1" without crashing the main test process
+ *
+ * @see <a href="https://lankydan.dev/running-a-kotlin-class-as-a-subprocess">https://lankydan.dev/running-a-kotlin-class-as-a-subprocess</a>
+ */
+@Throws(IOException::class, InterruptedException::class)
+fun execAppInSubprocess(args: List<String> = emptyList(), outputFileName: String): ProcessResult {
+    val javaHome = System.getProperty("java.home")
+    val javaBin = javaHome + File.separator + "bin" + File.separator + "java"
+    val classpath = System.getProperty("java.class.path")
+    val className = "datamaintain.cli.app.AppKt"
+
+    val command = ArrayList<String>()
+    command.add(javaBin)
+    command.add("-cp")
+    command.add(classpath)
+    command.add(className)
+    command.addAll(args)
+
+    // directly write output to the given output file
+    val process = ProcessBuilder(command)
+            .redirectOutput(File(outputFileName))
+            .redirectErrorStream(true)
+            .start()
+
+    process.waitFor()
+    return ProcessResult(process.exitValue(), "")
+}
