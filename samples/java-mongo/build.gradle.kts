5ee511a3092ee639de277f5adae3e25600bdcb2b
nroulon <nicolas.roulon@4sh.fr>
2022-10-27 12:18:12 +0200

    fix: Java sample does not use a driver-mongo-mapping

--- samples/java-mongo/build.gradle.kts
+++ samples/java-mongo/build.gradle.kts
@@ -11,6 +11,8 @@ repositories {
 dependencies {
     implementation(project(":modules:core"))
     implementation(project(":modules:driver-mongo"))
+    implementation(project(":modules:driver-mongo-mapping:driver-mongo-mapping-serialization"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:${Versions.kotlinxSerialization}")
     implementation("org.mongodb:mongodb-driver:3.11.2")
     implementation("org.jongo:jongo:${Versions.jongo}")
 }
