ce6d02f573200610531e2815eb522dba99ac7bd5
Élise Rubio <30841868+Lysoun@users.noreply.github.com>
2023-01-24 16:05:06 +0100

    :sparkles: Send start & stop execution

--- modules/domain-report/src/main/kotlin/datamaintain/domain/report/IExecutionWorkflowMessagesSender.kt
+++ modules/domain-report/src/main/kotlin/datamaintain/domain/report/IExecutionWorkflowMessagesSender.kt
@@ -1,5 +1,8 @@
 package datamaintain.domain.report
 
+typealias ExecutionId = Int
+
 interface IExecutionWorkflowMessagesSender {
-    fun sendReport(report: Report)
+    fun startExecution(): ExecutionId
+    fun sendReport(executionId: ExecutionId, report: Report)
 }
--- modules/monitoring/src/main/kotlin/datamaintain/monitoring/ExecutionWorkflowMessagesSender.kt
+++ modules/monitoring/src/main/kotlin/datamaintain/monitoring/ExecutionWorkflowMessagesSender.kt
@@ -1,18 +1,31 @@
 package datamaintain.monitoring
 
+import ExecutionStartResponse
 import MonitoringReport
+import datamaintain.domain.report.ExecutionId
 import datamaintain.domain.report.IExecutionWorkflowMessagesSender
 import datamaintain.domain.report.Report
 import org.http4k.client.Java8HttpClient
+import org.http4k.core.Body
 import org.http4k.core.Method
 import org.http4k.core.Request
+import org.http4k.format.Jackson.auto
 
 class ExecutionWorkflowMessagesSender : IExecutionWorkflowMessagesSender {
     private val httpClient = Java8HttpClient()
     private val executionApiBaseUrl = "$baseUrl/public/executions"
 
-    override fun sendReport(report: Report) {
-        httpClient(Request(Method.POST, "$executionApiBaseUrl/report").body(report.toMonitoringReport()))
+    override fun startExecution(): ExecutionId =
+        httpClient(Request(Method.POST, "$executionApiBaseUrl/start"))
+            .let(executionStartResponse)
+            .executionId
+
+    override fun sendReport(executionId: ExecutionId, report: Report) {
+        httpClient(Request(Method.PUT, "$executionApiBaseUrl/stop/$executionId").body(report.toMonitoringReport()))
+    }
+
+    companion object {
+        val executionStartResponse = Body.auto<ExecutionStartResponse>().toLens()
     }
 }
 
