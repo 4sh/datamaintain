b2578cd961c313acb9a56ab35129f746d44ed01f
Élise Rubio <30841868+Lysoun@users.noreply.github.com>
2023-02-07 10:38:56 +0100

    Use composite build to read monitoring api dependency

--- modules/monitoring/build.gradle.kts
+++ modules/monitoring/build.gradle.kts
@@ -5,11 +5,6 @@ plugins {
     id("com.adarshr.test-logger")
 }
 
-repositories {
-    mavenLocal()
-    mavenCentral()
-}
-
 baseProject()
 
 publishing {
@@ -26,10 +21,8 @@ publishing {
 
 dependencies {
     implementation(project(":modules:domain-report"))
-    implementation("io.github.4sh.datamaintain-monitoring:api-execution-report:83d6333")
+    implementation("io.github.4sh.datamaintain-monitoring:api-execution-report:DEV")
     implementation("org.http4k:http4k-core:4.3.4.1")
     implementation("org.http4k:http4k-format-jackson:4.3.4.1")
-
-    testImplementation("ch.qos.logback:logback-classic:${Versions.logbackClassic}")
 }
 
