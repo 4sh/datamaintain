9676e52e7bfcd0cad1e3251216c3f3fe74334041
Élise Rubio <30841868+Lysoun@users.noreply.github.com>
2023-03-21 09:31:21 +0100

    :recycle: Extract mock start answer set up to method

--- modules/test/src/test/kotlin/datamaintain/test/MonitoringSendHttp4KIT.kt
+++ modules/test/src/test/kotlin/datamaintain/test/MonitoringSendHttp4KIT.kt
@@ -16,9 +16,7 @@ import strikt.assertions.isSuccess
  * When monitoring configuration is given an url for monitoring, Executor should send information about
  * the current batch to the remote monitoring server
  */
-class MonitoringSendHttp4KIT: AbstractMonitoringSendWithHttpTest() {
-    private val executionId: ExecutionId = 42
-
+class MonitoringSendHttp4KIT : AbstractMonitoringSendWithHttpTest() {
     @Nested
     inner class MonitoringIsUnreachable {
         @Test
@@ -32,11 +30,7 @@ class MonitoringSendHttp4KIT: AbstractMonitoringSendWithHttpTest() {
     inner class MonitoringIsReachable {
         @BeforeEach
         fun setupStartAnswer() {
-            mockServerClient.`when`(
-                request()
-                    .withMethod("POST")
-                    .withPath("/public/executions/start")
-            ).respond(response().withBody("{\"executionId\": $executionId}"))
+            setupMockStartAnswer(42)
         }
 
         @Test
@@ -47,6 +41,14 @@ class MonitoringSendHttp4KIT: AbstractMonitoringSendWithHttpTest() {
             // Then
             mockServerClient.verify(request().withPath("/public/executions/start"))
         }
+    }
+
+    private fun setupMockStartAnswer(executionId: ExecutionId) {
+        mockServerClient.`when`(
+            request()
+                .withMethod("POST")
+                .withPath("/public/executions/start")
+        ).respond(response().withBody("{\"executionId\": $executionId}"))
     }
 
     private fun buildDatamaintainWithMonitoringConfiguration() = Datamaintain(
