31e9fc4c10ab065be659c1eb726d571b2e49d660
Ivan “CLOVIS” Canet <ivan.canet@4sh.fr>
2023-10-11 16:02:52 +0200

    Inline the common dependency configuration

--- modules/test/build.gradle.kts
+++ modules/test/build.gradle.kts
@@ -3,7 +3,29 @@ plugins {
     id("com.adarshr.test-logger")
 }
 
-baseProject()
+dependencies {
+    "api"((this.platform("org.jetbrains.kotlin:kotlin-bom:${Versions.kotlin}")))
+    "implementation"("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+dependencies {
+    "implementation"("io.github.microutils:kotlin-logging:${Versions.kotlinLogging}")
+
+    "testImplementation"("org.junit.jupiter:junit-jupiter-api:${Versions.junit}")
+    "testImplementation"("org.junit.jupiter:junit-jupiter-params:${Versions.junit}")
+    "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:${Versions.junit}")
+    "testImplementation"("io.strikt:strikt-core:${Versions.strikt}")
+    "testImplementation"("io.mockk:mockk:${Versions.mockk}")
+    "testImplementation"("org.testcontainers:testcontainers:${Versions.testcontainers}")
+    "testImplementation"("org.testcontainers:junit-jupiter:${Versions.testcontainers}")
+    "testImplementation"("org.testcontainers:mongodb:${Versions.testcontainers}")
+}
+
+tasks.getByPath("test").doFirst({
+    with<org.gradle.api.tasks.testing.Test, kotlin.Unit>(this as Test) {
+        this.useJUnitPlatform()
+    }
+})
 
 kotlin {
     jvmToolchain(17)
