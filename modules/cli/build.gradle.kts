c1f18fd2523fcc4312a0ec3e4850dab9d692a0da
Ivan “CLOVIS” Canet <ivan.canet@4sh.fr>
2023-10-11 17:52:53 +0200

    Extract the common dependencies to the 'kotlin' conventions

--- modules/cli/build.gradle.kts
+++ modules/cli/build.gradle.kts
@@ -12,20 +12,6 @@ plugins {
 }
 
 dependencies {
-    api(platform(libs.kotlin.bom))
-    implementation(libs.kotlin.jdk8)
-
-    implementation(libs.kotlinLogging)
-
-    testImplementation(libs.junit.jupiter.api)
-    testImplementation(libs.junit.jupiter.engine)
-    testRuntimeOnly(libs.junit.jupiter.engine)
-    testImplementation(libs.strikt)
-    testImplementation(libs.mockk)
-    testImplementation(libs.testContainers)
-    testImplementation(libs.testContainers.jupiter)
-    testImplementation(libs.testContainers.mongodb)
-
     implementation(projects.modules.core)
     implementation(projects.modules.driverMongo)
     implementation(projects.modules.driverMongoMapping.driverMongoMappingSerialization)
