31a1d648a95b96e0489a37d03ad0d583291fd760
Ivan “CLOVIS” Canet <ivan.canet@4sh.fr>
2023-10-11 18:56:34 +0200

    Migrate the MavenCentral publication logic to its own convention plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,22 +3,4 @@ plugins {
     alias(libs.plugins.palantir.graal) apply false
     alias(libs.plugins.palantir.git)
     alias(libs.plugins.testLogger) apply false
-    id("maven-publish")
-    signing
-}
-
-val modulesToPublish = listOf(
-    "core",
-    "driver-jdbc",
-    "driver-mongo",
-    "driver-mongo-mapping-serialization",
-    "driver-mongo-mapping-jackson",
-    "driver-mongo-mapping-gson",
-    "driver-mongo-mapping-test"
-)
-
-allprojects {
-    if (modulesToPublish.contains(this.name)) {
-        apply(from = rootProject.file("buildScripts/gradle/publishing.gradle.kts"))
-    }
 }
