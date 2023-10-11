ae583c7a34f4c368ec1857b327f1b87e7ae17a51
Ivan “CLOVIS” Canet <ivan.canet@4sh.fr>
2023-10-11 17:44:19 +0200

    Extract the group and version configuration to the 'kotlin' conventions

---
+++ gradle/conventions/base/src/main/kotlin/kotlin.gradle.kts
@@ -0,0 +1,31 @@
+package datamaintain.conventions
+
+import org.gradle.accessors.dm.LibrariesForLibs
+
+// Access the version catalog inside the plugin
+// See https://github.com/gradle/gradle/issues/15383#issuecomment-779893192
+val libs = the<LibrariesForLibs>()
+
+plugins {
+    kotlin("jvm")
+    id("com.palantir.git-version")
+}
+
+group = "io.github.4sh.datamaintain"
+
+// region Version generation
+
+val versionDetails: groovy.lang.Closure<com.palantir.gradle.gitversion.VersionDetails> by extra
+val lastTag: String = versionDetails().lastTag
+
+version = if (lastTag != "") {
+    lastTag
+} else {
+    "SNAPSHOT"
+}
+
+// endregion
+
+repositories {
+    mavenCentral()
+}
