a17f538a01d35f534a4f5c664d743894c12de2bb
Ivan “CLOVIS” Canet <ivan.canet@4sh.fr>
2023-10-11 19:12:35 +0200

    Move the testLogger plugin into the 'kotlin' convention plugin

--- modules/driver-mongo/build.gradle.kts
+++ modules/driver-mongo/build.gradle.kts
@@ -2,8 +2,6 @@ plugins {
     id("datamaintain.conventions.kotlin")
     id("datamaintain.conventions.publishing")
     id("datamaintain.conventions.driver")
-
-    alias(libs.plugins.testLogger)
 }
 
 dependencies {
