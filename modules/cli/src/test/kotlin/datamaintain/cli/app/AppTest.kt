7612ec1de0570cfbc2b3d854f26bfabbf16bb2f7
Ivan “CLOVIS” Canet <ivan.canet@4sh.fr>
2023-10-11 15:34:48 +0200

    Do not fail tests when the locale is not english

--- modules/cli/src/test/kotlin/datamaintain/cli/app/AppTest.kt
+++ modules/cli/src/test/kotlin/datamaintain/cli/app/AppTest.kt
@@ -43,7 +43,7 @@ internal class AppTest : BaseCliTest() {
 
                 // Then
                 expectThat(exitCode).isEqualTo(1)
-                expectThat(output).contains("java.io.FileNotFoundException: non-existing-file.properties (No such file or directory)")
+                expectThat(output).contains("java.io.FileNotFoundException: non-existing-file.properties")
             }
 
             @Test
